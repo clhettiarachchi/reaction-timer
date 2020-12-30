d4c6176d9f96f384b544bb7ae11754d46b4f08c0
clhettiarachchi <chandikalakshan@gmail.com>
2020-12-30 12:14:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 // Serve all the files in '/dist' directory
 app.use(express.static('dist'));
 
+app.get('/', (req, res) => {
+   res.render('index.html')
+});
 
 app.listen(3000, function () {
   console.log('Reaction Timer app listening on port 3000!');
